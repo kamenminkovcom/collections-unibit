a81914c4bd1e9fc0b8bcfabce37182f0eaa8c9ab
Kamnen Minkov <kamenminkov.com@gmail.com>
2022-05-14 17:46:47 +0300

    add People problem solution

---
+++ People/Person.cs
@@ -0,0 +1,17 @@
+﻿
+namespace People
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+
+        public Person(string name, int age, string occupation)
+        {
+            Age = age;
+            Name = name;
+            Occupation = occupation;
+        }
+    }
+}
--- People/Program.cs
+++ People/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace People
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string STOP_COMMAND = "STOP";
+            int YOUTH_LIMIT = 35;
+            List<Person> people = new List<Person>();
+
+            string searchOccupation = Console.ReadLine();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == STOP_COMMAND)
+                {
+                    break;
+                }
+
+                string[] personInfo = input.Split(' ');
+                people.Add(new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]));
+            }
+
+            List<string> searchPeopleNames = people
+                .Where(person => person.Age <= YOUTH_LIMIT && person.Occupation == searchOccupation)
+                .Select(person => person.Name)
+                .OrderBy(name => name)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", searchPeopleNames));
         }
     }
 }
